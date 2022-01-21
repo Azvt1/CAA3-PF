d3aff03156154d6c1e4433e16bff12f23c8d90a3
Azat0907 <72181175+Azat0907@users.noreply.github.com>
2022-01-21 12:53:59 +0800

    Add files via upload

---
+++ HawkerFoodStall.java
@@ -0,0 +1,182 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HawkerFoodStall {
+    private String stallName;
+    private String description;
+    private String ownerName;
+    private String address;
+    private int postal;
+    private String preference;
+    private ArrayList<StallFoodMenu> stallFoodMenuList = new ArrayList<StallFoodMenu>();
+
+    // Arraylist for containing new Stalls
+    static List<HawkerFoodStall> StallsList = new ArrayList<HawkerFoodStall>();
+    static String[] FoodPreferences = new String[]{"spicy, salty, bitter, sweet"};
+
+
+
+    // public HawkerFoodStall(){};+
+    // Constructor
+    public HawkerFoodStall(String stallName,String description,String ownerName,String address, int postal) {
+        this.stallName = stallName;
+        this.description = description;
+        this.ownerName = ownerName;
+        this.address = address;
+        this.postal = postal;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Name = " + stallName + ", description = " + description + ", owner Name = " + ownerName + ", " +
+                "address = " + address + ", postal = " + postal + ", menu = " + stallFoodMenuList;
+    }
+    // Set & Get method for stallName
+    public void setStallName(String stallName) {
+        this.stallName = stallName;
+    }
+    public String getStallName() {
+        return stallName;
+    }
+
+    // Set & Get method for description
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    // Set & Get method for Owner name
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    // Set & Get methods for Address
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    // Set & get methods for postal
+    public void setPostal(int postal) {this.postal = postal;}
+    public int getPostal() {
+        return postal;
+    }
+    public void setFoodPreferences(String preference) {
+        this.preference = preference;
+    }
+
+    // Get method for Hawker list
+    public void setStallFoodMenuList(ArrayList<StallFoodMenu> stallFoodMenuList) {
+        this.stallFoodMenuList = stallFoodMenuList;
+    }
+    public ArrayList<StallFoodMenu> getStallFoodMenuList() {
+        return stallFoodMenuList;
+    }
+
+    public void addNewHawkerFoodStall() {
+        HawkerFoodStall h1 = new HawkerFoodStall("", "", "", "", 0);
+        Scanner newHawker = new Scanner(System.in);
+        Scanner foodName = new Scanner(System.in);
+        Scanner calories = new Scanner(System.in);
+        Scanner price = new Scanner(System.in);
+
+
+        System.out.println("Here we go! Hawker should have 'name', 'description', 'ownerName', 'address', 'postal' and Menu list");
+        System.out.print("Now, enter the name: ");
+        String givenName = newHawker.nextLine();
+        h1.setStallName(givenName);
+
+        System.out.print("Now, enter the description: ");
+        String givenDescription = newHawker.nextLine();
+        h1.setDescription(givenDescription);
+
+        System.out.print("Now, enter the ownerName: ");
+        String givenOwnerName = newHawker.nextLine();
+        h1.setOwnerName(givenOwnerName);
+
+        System.out.print("Now, enter the address: ");
+        String givenAddress = newHawker.nextLine();
+        h1.setAddress(givenAddress);
+
+        System.out.print("Now enter the postal: ");
+        int givenPostal = newHawker.nextInt();
+        h1.setPostal(givenPostal);
+
+        System.out.println("Now, can you please enter type that match your Hawker Stall. If you don't want, type something" +
+                "else");
+        System.out.println(FoodPreferences.toString());
+        System.out.println("""
+                Now, write your menu, each meal should contain 'food name', 'calories', 'price'.
+                                       -----IMPORTANT-----
+                When you want to finish creating menu just type 'stop', when food name will be asked again.
+                """);
+        while (true) {
+            StallFoodMenu s1 = new StallFoodMenu("", 0, 0);
+            System.out.print("Enter food name: ");
+            String givenFoodName = foodName.nextLine();
+            if (givenFoodName.equals("stop")) {
+                break;
+            }
+            else {
+                s1.setFoodName(givenFoodName);
+
+                System.out.print("Enter calories of the food: ");
+                double givenCalories = calories.nextDouble();
+                s1.setCalories(givenCalories);
+
+                System.out.print("Enter price: ");
+                double givenPrice = price.nextDouble();
+                s1.setPrice(givenPrice);
+
+                h1.stallFoodMenuList.add(s1);
+                // DO A SET method FOR THE MENU
+            }
+
+        }
+        StallsList.add(h1);
+        System.out.println("** Hawker stall was successfully added! **");
+        System.out.println("Your Hawker stall looks like:");
+        System.out.println(h1);
+    }
+
+    public void deleteHawkerFoodStall() {
+        Scanner deleteHawker = new Scanner(System.in);
+        System.out.println("Enter the name of Hawker stall that you want to delete: ");
+        for (int j = 0; j < StallsList.size(); j++) {
+            System.out.println(StallsList.get(j));
+        }
+        String deleteName = deleteHawker.nextLine().toLowerCase();
+        boolean wasNotDeleted = true;
+        for (int i = 0; i < StallsList.size(); i++) {
+            if (StallsList.get(i).getStallName().toLowerCase().equals(deleteName)) {
+                StallsList.remove(i);
+                stallFoodMenuList.clear();
+                System.out.println("Hawker food stall has been deleted!");
+                wasNotDeleted = false;
+                break;
+            }
+        }
+        if (wasNotDeleted) {
+            System.out.println("Hawker food stall was not deleted, because there is no such Hawker food stall =(");
+        }
+    }
+
+    public static void ShowAllHawkerStalls() {
+        for (int i = 0; i < StallsList.size(); i++) {
+            System.out.println(StallsList.get(i));
+        }
+    }
+
+
+}
